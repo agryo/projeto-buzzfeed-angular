9b6ab9f9edc0e5b090a378c32659ac490f5db332
Agryo Araujo <agryostallion@gmail.com>
2023-05-27 14:31:40 -0300

    Criação das variáveis para deixar dinâmico

--- buzzfeed-project/src/app/components/quizz/quizz.component.ts
+++ buzzfeed-project/src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  titulo:string = ""
+  perguntas:any
+  perguntaAtual:any
+  respostas:string[] = []
+  respostaEscolhida:string = ""
+  indexPergunta:number = 0
+  indexMaxPergunta:number = 0
+  terminou:boolean = false
+  
+  constructor(){ }
+
+  ngOnInit(): void {
+  }
 
 }
